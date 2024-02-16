491558beb4763a8d957b0e173d109b0d72ee0219
Luiz Henrique <luizhenrique.se@gmail.com>
2024-02-16 14:04:54 -0300

    feat: add connection pool database configs

--- src/banco/banco.go
+++ src/banco/banco.go
@@ -2,6 +2,7 @@ package banco
 
 import (
 	"database/sql"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql" // Driver
 )
@@ -11,11 +12,15 @@ func Conectar() (*sql.DB, error) {
 	//docker
 	stringConexao := "user:123456@tcp(mysqldocker:3306)/rinhabank?charset=utf8&parseTime=True&loc=Local"
 	//local mysql
-	//stringConexao := "root:123456@tcp(localhost:3306)/rinhabank?charset=utf8&parseTime=True&loc=Local" 
+	//stringConexao := "root:123456@tcp(localhost:3306)/rinhabank?charset=utf8&parseTime=True&loc=Local"
 	db, erro := sql.Open("mysql", stringConexao)
 	if erro != nil {
 		return nil, erro
 	}
+
+	db.SetConnMaxLifetime(time.Second * 10)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
 
 	if erro = db.Ping(); erro != nil {
 		db.Close()
